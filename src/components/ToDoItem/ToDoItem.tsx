869c4750ef570465ec7059bf9c4a972953c74f4c
Yaroslav Slepukhin <YaroslavSlepukhin@gmail.com>
2021-10-17 01:11:01 +0300

    add React.memo

--- src/components/ToDoItem/ToDoItem.tsx
+++ src/components/ToDoItem/ToDoItem.tsx
@@ -1,8 +1,8 @@
 import { Tag } from "antd";
-import { FC } from "react";
+import React, { FC } from "react";
 import ToDo from "../../store/ToDo";
 
-const ToDoList: FC<{ element: string }> = ({ element }) => {
+const ToDoList: FC<{ element: string }> = React.memo(({ element }) => {
     const onClose = () => {
         ToDo.removeToDoItem(element);
     };
@@ -12,6 +12,27 @@ const ToDoList: FC<{ element: string }> = ({ element }) => {
             {element}
         </Tag>
     );
-}
+})
 
 export default ToDoList;
+
+// import { Tag } from "antd";
+// import { FC } from "react";
+// import ToDo from "../../store/ToDo";
+
+// const ToDoList: FC<{ element: string }> = ({ element }) => {
+//     console.warn('RENDER');
+    
+
+//     const onClose = () => {
+//         ToDo.removeToDoItem(element);
+//     };
+
+//     return (
+//         <Tag closable onClose={onClose}>
+//             {element}
+//         </Tag>
+//     );
+// }
+
+// export default ToDoList;
