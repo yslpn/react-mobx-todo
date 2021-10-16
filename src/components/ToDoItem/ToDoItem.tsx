f086116106825b1ab024e105ffda3ae324a468e9
Yaroslav Slepukhin <YaroslavSlepukhin@gmail.com>
2021-10-17 01:13:21 +0300

    minor chages

--- src/components/ToDoItem/ToDoItem.tsx
+++ src/components/ToDoItem/ToDoItem.tsx
@@ -15,24 +15,3 @@ const ToDoList: FC<{ element: string }> = React.memo(({ element }) => {
 })
 
 export default ToDoList;
-
-// import { Tag } from "antd";
-// import { FC } from "react";
-// import ToDo from "../../store/ToDo";
-
-// const ToDoList: FC<{ element: string }> = ({ element }) => {
-//     console.warn('RENDER');
-    
-
-//     const onClose = () => {
-//         ToDo.removeToDoItem(element);
-//     };
-
-//     return (
-//         <Tag closable onClose={onClose}>
-//             {element}
-//         </Tag>
-//     );
-// }
-
-// export default ToDoList;
